d0608d45f2c03b87e62633e4aa3273865ce5aeda
riyap3197 <riyap3197@gmail.com>
2023-06-20 13:54:45 -0400

    added POST beer.

--- src/main/java/springframework/spring6restmvc/controller/BeerController.java
+++ src/main/java/springframework/spring6restmvc/controller/BeerController.java
@@ -2,11 +2,11 @@ package springframework.spring6restmvc.controller;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import springframework.spring6restmvc.model.Beer;
 import springframework.spring6restmvc.services.BeerService;
 
@@ -19,6 +19,14 @@ import java.util.UUID;
 @RequestMapping("/api/v1/beer")
 public class BeerController {
     private final BeerService beerService;
+
+    @PostMapping
+    public ResponseEntity handlePost(@RequestBody Beer beer){
+        Beer savedBeer = beerService.saveNewBeer(beer);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Location","/api/v1/beer/" + savedBeer.getId().toString());
+        return new ResponseEntity(headers, HttpStatus.CREATED);
+    }
 
     @RequestMapping(method = RequestMethod.GET)
     public List<Beer> listBeers(){
